9fd7c57cfc8cb6907702408ff1e10a6952143e78
RayverAimar <rayver.munoz@ucsp.edu.pe>
2023-03-09 13:12:18 -0500

    Fix variable scaling double fit_transform method used in train & test partitions

--- 1 - Data Preprocessing/dataset_loader_template.py
+++ 1 - Data Preprocessing/dataset_loader_template.py
@@ -44,4 +44,4 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
-X_test = sc_X.fit_transform(X_test)
+X_test = sc_X.transform(X_test) # just transform as it is already fitted
