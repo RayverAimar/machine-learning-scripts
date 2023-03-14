70a468748fa16b062902803e2220667bc8a7e930
RayverAimar <rayver.munoz@ucsp.edu.pe>
2023-03-14 18:47:31 -0500

    Add K-Means script plus visualization plots

--- 4 - Clustering/Section 24 - K-Means Clustering/kmeans.py
+++ 4 - Clustering/Section 24 - K-Means Clustering/kmeans.py
@@ -5,3 +5,46 @@ Created on Mon Mar 13 21:01:56 2023
 @author: Rayver
 """
 
+# K-Means
+
+# Import libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Load dataset
+dataset = pd.read_csv("Mall_Customers.csv")
+X = dataset.iloc[:, [3,4]].values
+
+# Elbow method to find the optime number of clusters
+from sklearn.cluster import KMeans
+wcss = []
+for i in range(1, 11):
+    kmeans = KMeans(n_clusters = i, init = "k-means++", max_iter = 300, n_init = 10, random_state = 0)
+    kmeans.fit(X)
+    wcss.append(kmeans.inertia_)
+
+plt.plot(range(1,11), wcss)
+plt.title("Elbow method")
+plt.xlabel("Number of clusters")
+plt.ylabel("WCSS(k)")
+plt.show()
+
+
+# Apply k-means method to the dataset
+kmeans = KMeans(n_clusters = 5, init="k-means++", max_iter = 300, n_init = 10, random_state = 0)
+y_kmeans = kmeans.fit_predict(X)
+
+
+# Visualization of clusters
+plt.scatter(X[y_kmeans == 0, 0], X[y_kmeans == 0, 1], s = 100, c = "red", label = "Cautious")
+plt.scatter(X[y_kmeans == 1, 0], X[y_kmeans == 1, 1], s = 100, c = "blue", label = "Standard")
+plt.scatter(X[y_kmeans == 2, 0], X[y_kmeans == 2, 1], s = 100, c = "green", label = "Objectives")
+plt.scatter(X[y_kmeans == 3, 0], X[y_kmeans == 3, 1], s = 100, c = "cyan", label = "Objective")
+plt.scatter(X[y_kmeans == 4, 0], X[y_kmeans == 4, 1], s = 100, c = "magenta", label = "Conservators")
+plt.scatter(kmeans.cluster_centers_[:,0], kmeans.cluster_centers_[:,1], s = 300, c = "yellow", label = "Baricenters")
+plt.title("Clients clusters")
+plt.xlabel("Anual income (miles of $)")
+plt.ylabel("Expense punctuation (1-100)")
+plt.legend()
+plt.show()
